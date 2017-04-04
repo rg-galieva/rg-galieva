22f6363acc616401aa5abe8071164051a0f2650c
Regina Galieva <rg.galieva@gmail.com>
2017-04-03 19:39:20 -0700

    slider settings into separate component

--- src/pages/prj-list/index.js
+++ src/pages/prj-list/index.js
@@ -1,53 +1,22 @@
-import React, {PropTypes, Component} from 'react'
+import React, {PropTypes} from 'react'
 import {connect} from 'react-redux'
 import ProjectPrev from '../../components/prj-prev'
 import Slider from 'react-slick'
-
-require('../../assets/styles/slick/slick.gcss');
+import config from '../../components/slider'
 import s from './_styles.pcss'
 
-const numb_0 = require('../../assets/svg/numbers/0.svg');
+const ProjectList = (props) => {
+    const projects = props.projects.map((project) => <div key={project.id}><ProjectPrev {...project}/></div>)
 
-const NextArrow = (props) => {
     return (
-        <div {...props} className={s.next}>NextArrow</div>
+        <Slider {...config}>
+            { projects }
+        </Slider>
     )
 }
 
-const PrevArrow = (props) => {
-    return (
-        <div {...props} className={s.prev}>PrevArrow</div>
-    )
-}
-
-class ProjectList extends Component {
-    render() {
-        const projects = this.props.projects.map((project) => <div key={project.id}><ProjectPrev {...project}/></div>)
-        const settings = {
-            dots: true,
-            infinite: true,
-            speed: 300,
-            slidesToShow: 1,
-            slidesToScroll: 1,
-            autoplay: true,
-            customPaging: function (i) {
-                const numb = require(`../../assets/svg/numbers/${i+1}.svg`);
-                return (
-                    <div className={s.slider}>
-                        <div dangerouslySetInnerHTML={{__html: `${numb_0} ${numb}`}} className={s.slider_numb}></div>
-                    </div>
-                )
-            },
-            nextArrow: <NextArrow/>,
-            prevArrow: <PrevArrow/>
-        };
-
-        return (
-            <Slider {...settings}>
-                { projects }
-            </Slider>
-        )
-    }
+ProjectList.PropTypes = {
+    projects: PropTypes.array.isRequired
 }
 
 const mapStateToProps = (state) => {
