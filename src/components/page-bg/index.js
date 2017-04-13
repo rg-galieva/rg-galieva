6278cf665560db2e4cfc332c9757dd7d27531972
Regina Galieva <rg.galieva@gmail.com>
2017-04-12 21:20:06 -0700

    about page - bg svg using PageBg component

---
+++ src/components/page-bg/index.js
@@ -0,0 +1,30 @@
+import React, {PropTypes, Component} from 'react'
+import isMobile from '../../utils/useragent-check'
+import {render} from 'react-dom';
+import s from './_styles.pcss'
+
+class PageBg extends Component {
+    componentDidMount() {
+        if (!isMobile) {
+            const bg = () => { return <div dangerouslySetInnerHTML={{__html: require(`./svg/${this.props.bg}.svg`)}} className={s.svg_bg}></div> }
+
+            setTimeout(() => {
+                    render(bg(), document.getElementById(this.props.page_node))
+                }, 350
+            )
+        }
+    }
+
+    render() {
+        return (
+            <div id={this.props.page_node}></div>
+        );
+    }
+}
+
+PageBg.PropTypes = {
+    page_node: PropTypes.string.isRequired,
+    bg: PropTypes.string.isRequired
+}
+
+export default PageBg;
--- src/pages/about/index.js
+++ src/pages/about/index.js
@@ -1,51 +1,37 @@
-import React, {Component} from 'react'
-import {findDOMNode} from 'react-dom';
+import React from 'react'
 import {FormattedHTMLMessage, FormattedMessage} from 'react-intl';
+import PageBg from '../../components/page-bg'
 import s from './_styles.pcss'
 
-const sf = require('./svg/sf.svg');
+const About = () => {
 
-class About extends Component {
-    constructor(props) {
-        super(props)
-    }
+    return (
+        <div className={s.wrap}>
+            <section className={['row', s.cont].join(' ')}>
+                <header className="col_4">
+                    <h2><FormattedMessage id="welcome"/></h2>
+                </header>
 
-    componentDidMount() {
-        setTimeout(() => {
-                findDOMNode(this.refs.sf_svg).className += " is_act";
-            }, 350
-        )
-    }
+                <div className="col_10">
+                    <div className={s.desc}>
+                        <p><FormattedHTMLMessage id="about.part1"/></p>
 
-    render() {
-        return (
-            <div className={s.wrap}>
-                <div dangerouslySetInnerHTML={{__html: sf}} className={s.sf_svg} ref="sf_svg"></div>
+                        <p><FormattedMessage id="about.part2"/></p>
 
-                <section className={['row', s.cont].join(' ')}>
-                    <header className="col_4">
-                        <h2><FormattedMessage id="welcome"/></h2>
-                    </header>
+                        <p><FormattedMessage id="about.part3"/></p>
 
-                    <div className="col_10">
-                        <div className={s.desc}>
-                            <p><FormattedHTMLMessage id="about.part1"/></p>
+                        <p><FormattedMessage id="about.part4"/></p>
 
-                            <p><FormattedMessage id="about.part2"/></p>
+                        <p><FormattedMessage id="about.part5"/></p>
 
-                            <p><FormattedMessage id="about.part3"/></p>
-
-                            <p><FormattedMessage id="about.part4"/></p>
-
-                            <p><FormattedMessage id="about.part5"/></p>
-
-                            <p><FormattedMessage id="about.part6"/></p>
-                        </div>
+                        <p><FormattedMessage id="about.part6"/></p>
                     </div>
-                </section>
-            </div>
-        )
-    }
+                </div>
+            </section>
+
+            <PageBg bg="about_bg" page_node="about_bg"/>
+        </div>
+    )
 }
 
 export default About
