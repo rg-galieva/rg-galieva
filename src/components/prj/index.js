996538675ad5e22c3fd2d88ead3693396b9f49a9
Regina Galieva <rg.galieva@gmail.com>
2018-07-24 13:33:10 -0700

    a new project: shipper rfq (fr8star)

--- src/components/prj/index.js
+++ src/components/prj/index.js
@@ -1,4 +1,4 @@
-import React, {PropTypes, Component} from 'react'
+import React, {Component, PropTypes} from 'react'
 import {FormattedMessage} from 'react-intl';
 import {connect} from 'react-redux'
 import {Link} from 'react-router-dom'
@@ -14,105 +14,113 @@ const icons = require('../../assets/svg/icons.svg');
 
 
 class Project extends Component {
-    getImage = (src) => {
-        return require("../../assets/img/projects/" + src);
-    };
+    getImage = src => require("../../assets/img/projects/" + src);
 
-    getBgImg = (src) => {
-        return {backgroundImage: 'url(' + this.getImage(src) + ')'}
-    };
+    getBgImg = src => ({backgroundImage: 'url(' + this.getImage(src) + ')'});
 
-    config = (this.props.isMobile) ? configMobile : configDesktop;
+    config = this.props.isMobile ? configMobile : configDesktop;
 
-    sliderDesktop = () => {
-        const img_list = this.props.active_project.slider_desktop;
-
+    sliderDesktop = img_list => {
         if (img_list.length > 1) {
             return (
-                <div>
-                    <h2><FormattedMessage id="desktop"/></h2>
-                    <div className={s.screens}>
-                        <ImageListSlider images={img_list} config={this.config}/>
-                    </div>
-                </div>
+              <div>
+                  <h2><FormattedMessage id="desktop"/></h2>
+                  <div className={s.screens}>
+                      <ImageListSlider images={img_list} config={this.config}/>
+                  </div>
+              </div>
             )
         } else if (img_list.length === 1) {
             return (
-                <div>
-                    <h2><FormattedMessage id="desktop"/></h2>
-                    <div className={s.screens}>
-                        <Image cloudName={CLOUD_PATH} publicId={`${CLOUD_PATH}/${img_list[0]}`}/>
-                    </div>
-                </div>
+              <div>
+                  <h2><FormattedMessage id="desktop"/></h2>
+                  <div className={s.screens}>
+                      <Image cloudName={CLOUD_PATH} publicId={`${CLOUD_PATH}/${img_list[0]}`}/>
+                  </div>
+              </div>
             )
         }
     }
 
-    sliderMobile = () => {
-        const img_list = this.props.active_project.slider_mobile;
-
+    sliderMobile = img_list => {
         if (img_list.length > 1) {
             return (
-                <div>
-                    <h2><FormattedMessage id="mobile"/></h2>
-                    <div className={s.screens_mobile}>
-                        <ImageListSlider images={img_list} config={this.config}/>
-                    </div>
-                </div>
+              <div>
+                  <h2><FormattedMessage id="mobile"/></h2>
+                  <div className={s.screens_mobile}>
+                      <ImageListSlider images={img_list} config={this.config}/>
+                  </div>
+              </div>
             )
-        } else if (img_list.length === 1)  {
+        } else if (img_list.length === 1) {
             return (
-                <div>
-                    <h2><FormattedMessage id="mobile"/></h2>
-                    <div className={s.screens_mobile}>
-                        <Image cloudName={CLOUD_PATH} publicId={`${CLOUD_PATH}/${img_list[0]}`}/>
-                    </div>
-                </div>
+              <div>
+                  <h2><FormattedMessage id="mobile"/></h2>
+                  <div className={s.screens_mobile}>
+                      <Image cloudName={CLOUD_PATH} publicId={`${CLOUD_PATH}/${img_list[0]}`}/>
+                  </div>
+              </div>
             )
         }
     }
 
 
     render() {
-        const {id, title, about_prj, prj_link, year, pic_full, place, stack, description} = this.props.active_project;
+        const {active_project} = this.props;
 
-        const descr_html = (description).map((d) => <li key={d}><FormattedMessage id={d}/></li>)
+        const {
+            id,
+            title,
+            about_prj,
+            role = "frontend_developer",
+            prj_link,
+            year,
+            pic_full,
+            place,
+            stack,
+            description,
+            slider_desktop,
+            slider_mobile,
+        } = active_project;
+
+        const descr_html = description.map(d => <li key={d}><FormattedMessage id={d}/></li>)
 
         return (
-            <div className={s.page}>
-                <div className={s.head} style={this.getBgImg(pic_full)}>
-                    <Link className={s.back_btn} to={`/projects/${id}`}>
-                        <svg viewBox="0 0 60 60">
-                            <use xlinkHref={`${icons}#close`}/>
-                        </svg>
-                    </Link>
+          <div className={s.page}>
+              <div className={s.head} style={this.getBgImg(pic_full)}>
+                  <Link className={s.back_btn} to={`/projects/${id}`}>
+                      <svg viewBox="0 0 60 60">
+                          <use xlinkHref={`${icons}#close`}/>
+                      </svg>
+                  </Link>
 
-                    <h1><FormattedMessage id={title}/></h1>
-                </div>
+                  <h1><FormattedMessage id={title}/></h1>
+              </div>
 
-                <div className={s.cont}>
-                    <div className="row">
-                        <div className="col_8">
-                            <div className={s.about}>
-                                <p><FormattedMessage id={about_prj}/></p><br />
-                                <p className="is-emph"><FormattedMessage id="techstack_2"/>: {stack}</p>
-                            </div>
+              <div className={s.cont}>
+                  <div className="row">
+                      <div className="col_8">
+                          <div className={s.about}>
+                              <p><FormattedMessage id={about_prj}/></p><br/>
+                              <p className="is-emph"><FormattedMessage id="techstack_2"/>: {stack}
+                              </p>
+                          </div>
 
-                            <ul>
-                                {descr_html}
-                            </ul>
-                        </div>
-                        <div className="col_4">
-                            <h5><FormattedMessage id="frontend_developer"/></h5>
-                            <p className="is-emph">{year} <FormattedMessage id={place}/></p>
-                            <a href={prj_link} target="_blank">{prj_link}</a>
-                        </div>
-                    </div>
-                </div>
+                          <ul>
+                              {descr_html}
+                          </ul>
+                      </div>
+                      <div className="col_4">
+                          <h5><FormattedMessage id={role}/></h5>
+                          <p className="is-emph">{year} <FormattedMessage id={place}/></p>
+                          <a href={prj_link} target="_blank">{prj_link}</a>
+                      </div>
+                  </div>
+              </div>
 
-                {this.sliderDesktop()}
-                {this.sliderMobile()}
-            </div>
+              {this.sliderDesktop(slider_desktop)}
+              {this.sliderMobile(slider_mobile)}
+          </div>
         )
     }
 }
